a4e5deed0501db3ddeb86b6b22ffe6048dc99150
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-04-20 02:21:41 -0300

    Migrate to debugging logs

--- src/frontend/lexical-analysis/flex-actions.c
+++ src/frontend/lexical-analysis/flex-actions.c
@@ -7,53 +7,53 @@
  */
 
 void IgnoredPatternAction(const char * lexeme) {
-	printf("IgnoredPatternAction: '%s'.\n", lexeme);
+	LogDebug("IgnoredPatternAction: '%s'.\n", lexeme);
 }
 
 TokenID UnknownPatternAction(const char * lexeme) {
-	printf("UnknownPatternAction: '%s' que es %d.\n", lexeme, *lexeme);
+	LogDebug("UnknownPatternAction: '%s' que es %d.\n", lexeme, *lexeme);
 	return YYUNDEF;
 }
 
 TokenID KeywordPatternAction(const char * lexeme, TokenID token) {
-	printf("KeywordPatternAction: '%s'.\n", lexeme);
+	LogDebug("KeywordPatternAction: '%s'.\n", lexeme);
 	return token;
 }
 
 TokenID BooleanPatternAction(const char * lexeme, char value) {
-	printf("BooleanPatternAction: '%s'.\n", lexeme);
+	LogDebug("BooleanPatternAction: '%s'.\n", lexeme);
 	// yylval = value;
 	return BOOLEAN;
 }
 
 TokenID CharPatternAction(const char * lexeme) {
-	printf("CharPatternAction: '%s'.\n", lexeme);
+	LogDebug("CharPatternAction: '%s'.\n", lexeme);
 	// yylval = *lexeme;
 	return CHAR;
 }
 
 TokenID IntegerPatternAction(const char * lexeme) {
-	printf("IntegerPatternAction: '%s'.\n", lexeme);
+	LogDebug("IntegerPatternAction: '%s'.\n", lexeme);
 	// yylval = atoi(lexeme);
 	return INTEGER;
 }
 
 TokenID StringPatternAction(const char * lexeme) {
-	printf("StringPatternAction: '%s'.\n", lexeme);
+	LogDebug("StringPatternAction: '%s'.\n", lexeme);
 	return STRING;
 }
 
 TokenID DecimalPatternAction(const char * lexeme) {
-	printf("DecimalPatternAction: '%s'.\n", lexeme);
+	LogDebug("DecimalPatternAction: '%s'.\n", lexeme);
 	return DECIMAL;
 }
 
 TokenID IndentationPatternAction(TokenID token) {
-	printf("IndentationPatternAction: '%s'.\n", (token == INDENT) ? "INDENT" : "DEDENT");
+	LogDebug("IndentationPatternAction: '%s'.\n", (token == INDENT) ? "INDENT" : "DEDENT");
 	return DECIMAL;
 }
 
 TokenID EndOfLinePatternAction(TokenID token) {
-	printf("EndOfLinePatternAction.\n");
+	LogDebug("EndOfLinePatternAction.\n");
 	return token;
 }
